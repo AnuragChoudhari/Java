513dca154154c43b9e85f215e6289333d37478f9
AnuragChoudhari <choudharianurag03@gmail.com>
2023-09-28 19:27:28 +0530

    listnode

---
+++ Arrays/Listnode/Solution.java
@@ -0,0 +1,33 @@
+package Arrays.Listnode;
+
+import java.util.List;
+
+class ListNode{
+    int val;
+    ListNode next;
+
+    public ListNode(int val){
+        this.val = val;
+    }
+}
+
+
+
+class Main{
+    public static void main(String[] args) {
+    ListNode node1 = new ListNode(10);
+    ListNode node2 = new ListNode(20);
+    ListNode node3 = new ListNode(30);
+
+    node1.next = node2;
+    node2.next = node3;
+
+    ListNode current = node1;
+
+    while(current!=null){
+        System.out.println(current.val);
+        current=current.next;
+    }
+
+    }
+}
